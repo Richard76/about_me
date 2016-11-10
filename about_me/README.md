ed025a9af5a28d2fcf5af1bb8fda8a219977e0f6
Richard Farr <richardfarr7@gmail.com>
2016-11-09 17:57:32 -0800

    homework day 3

---
+++ about_me/README.md
@@ -0,0 +1,3 @@
+# about_me
+
+This is a web page about me
---
+++ about_me/orig.js
@@ -0,0 +1,76 @@
+'use strict';
+
+
+    /*
+    These are the questions I will ask
+    Are You Male?
+    Are You Over 18?
+    Did you register to vote?
+    Did you vote?
+    Did you vote to have the queen of England be the president of the US?
+    */
+
+var q1Male = prompt('Are you male?');
+console.log('User responded to the male Q with: ' + q1Male );
+
+if (q1Male.toLowerCase() === 'yes' ||
+  q1Male.toLowerCase() === 'y') {
+  alert('Sweet, me too!');
+} else if (q1Male.toLowerCase() === 'no' ||
+q1Male.toLowerCase() === 'n') {
+  alert('OK, you must be a woman then');
+} else {
+  alert('Oh, OK then.');
+}
+
+var q2Over18 = prompt('Are you Over 18?');
+console.log('User responded to the Over 18 question with: ' + q2Over18 );
+
+if (q2Over18.toLowerCase() === 'yes' ||
+  q2Over18.toLowerCase() === 'y') {
+  alert('Good, me too! We can both vote');
+} else if (q2Over18.toLowerCase() === 'no' ||
+q2Over18.toLowerCase() === 'n') {
+  alert('Good. You can not Vote');
+} else {
+  alert('I do not understand your answer');
+}
+
+var q3RegisterVote = prompt('Did you register to vote?');
+console.log('User responded to the register to vote question with: ' + q3RegisterVote );
+
+if (q3RegisterVote.toLowerCase() === 'yes' ||
+  q3RegisterVote.toLowerCase() === 'y') {
+  alert('That makes both of us');
+} else if (q3RegisterVote.toLowerCase() === 'no' ||
+q3RegisterVote.toLowerCase() === 'n') {
+  alert('You should have');
+} else {
+  alert('I do not understand your answer here. say yes or no next time I ask you a question');
+}
+
+var q4DidVote = prompt('Did you vote?');
+console.log('User responded to the did you vote question with: ' + q4DidVote );
+
+if (q4DidVote.toLowerCase() === 'yes' ||
+  q4DidVote.toLowerCase() === 'y') {
+  alert('Sweet, me too!');
+} else if (q4DidVote.toLowerCase() === 'no' ||
+q4DidVote.toLowerCase() === 'n') {
+  alert('You should have');
+} else {
+  alert('You are not very good at answering yes no questions.');
+}
+
+var q5VoteQueen = prompt('Did you vote for the queen to be president of the US?');
+console.log('User responded to did you vote for the queen question with: ' + q5VoteQueen );
+
+if (q5VoteQueen.toLowerCase() === 'yes' ||
+  q5VoteQueen.toLowerCase() === 'y') {
+  alert('I agree. She is a great ruler. Beats the pope almost any day.');
+} else if (q5VoteQueen.toLowerCase() === 'no' ||
+q5VoteQueen.toLowerCase() === 'n') {
+  alert('You should have. She rules.');
+} else {
+  alert('You really are bad at answering yes no questions. Next time vote for the queen. she rocks.');
+}
